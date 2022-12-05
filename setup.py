7412f3f53066759fe79c7496580211f5ae489fb2
Tinggao <Cui>
2022-12-05 05:48:05 +0100

    feature:the features for draw the snowflake ,the task4 of homework5

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name= "snowflake",
+    version = "0.1",
+    author="Tinggao Cui",
+    author_email = "tginfo.cui@fau.de",
+    packages = find_packages(),
+    install_requires = ["numpy","turtles"],
+)
---
+++ snowflake/__main__.py
@@ -0,0 +1,4 @@
+from snowflake import let_it_snow
+
+if __name__ == '__main__':
+    let_it_snow.main()
